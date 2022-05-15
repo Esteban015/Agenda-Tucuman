b184589e9c9402b5039319cb122a459574da03de
Esteban015 <estebanamaya015@gmail.com>
2022-05-15 13:56:01 -0300

    Nuevos cambios en test suite

---
+++ Test Suites/Header/TS - Header.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Se agregaran todos los TC y TS asociados al Header del WebSite</description>
+   <name>TS - Header</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>100742c3-1bad-4f90-8fee-5bd71f8fa1a3</testSuiteGuid>
+   <testCaseLink>
+      <guid>3e2db1cd-db1e-4593-a642-8cd7ae2aa69a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Header/Menu Navbar Hoy</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
